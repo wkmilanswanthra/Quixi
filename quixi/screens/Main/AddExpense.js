d1062aaa2f9c7126d8e74494a6ac044d6a39d9df
Ishanka Perera <pererawcpi.20@itfac.mrt.ac.lk>
2023-03-29 06:52:30 +0530

    Major changes

--- quixi/screens/Main/AddExpense.js
+++ quixi/screens/Main/AddExpense.js
@@ -11,14 +11,17 @@ import * as SecureStore from "expo-secure-store";
 import {EXPENSE_ROUTES} from "../../assets/constants/routes";
 import Axios from "axios";
 
+
 export default function AddExpense({navigation}) {
+
+    // Define the initial state variables with the useState hook
     const [userId, setUserId] = useState('')
     const [token, setToken] = useState('')
     const [splitAmong, setSplitAmong] = useState([])
     const [paidBy, setPaidBy] = useState([])
     const [contribution, setContribution] = useState({})
 
-
+    // Define the useEffect hook, which will fetch the user ID and token when the component mounts
     useEffect(() => {
         async function fetchData() {
             await getToken();
@@ -33,14 +36,16 @@ export default function AddExpense({navigation}) {
             },])
         }
 
-        fetchData();
+        fetchData(); // Call the fetchData function when the component mounts
     }, [])
 
+    // Define a function to fetch the user ID from SecureStore and update the state
     const getUserId = async () => {
         const userId = await SecureStore.getItemAsync('userId');
         setUserId(userId);
     }
 
+    // Define a function to fetch the user's token from SecureStore and update the state
     const getToken = async () => {
         const token = await SecureStore.getItemAsync('token');
         setToken(token);
@@ -50,9 +55,13 @@ export default function AddExpense({navigation}) {
 
     }
 
+    // Define a function to create an expense using Axios and the API endpoint
     function createExpense() {
+        // Check if the contribution object exists
         if (contribution) {
+            // Define the API endpoint URL
             const url = EXPENSE_ROUTES.CREATE;
+            // Define the Axios configuration object with the necessary headers and data
             const config = {
                 method: 'post', url: url, headers: {
                     'Authorization': 'Bearer ' + token.replaceAll('"', ''),
@@ -66,6 +75,7 @@ export default function AddExpense({navigation}) {
                 })
             }
             console.log(config)
+            // Make a POST request to the API endpoint with the Axios configuration object
             Axios(config).then((response) => {
                 console.log('created group = ', response.data)
             }).catch((error) => {
@@ -74,6 +84,7 @@ export default function AddExpense({navigation}) {
         }
     }
 
+    // Define a function to handle the submit button click event
     const handleSubmit = () => {
         console.log('submit click', token, userId)
         if (token && userId) {
@@ -92,7 +103,7 @@ export default function AddExpense({navigation}) {
                     <View style={styles.firstContainer}>
                         <View style={styles.view2}>
                             <Text style={styles.text2}>Members</Text>
-                            <View style={[styles.view4, {marginTop: 30}]}>
+                            <View style={[styles.view4]}>
                                 <ScrollView style={styles.view3} horizontal={true}>
                                     <Image style={styles.img} source={{uri: 'https://picsum.photos/200/200'}}></Image>
                                     <Image style={styles.img} source={{uri: 'https://picsum.photos/200/200'}}></Image>
@@ -101,26 +112,35 @@ export default function AddExpense({navigation}) {
                                     <Image style={styles.img} source={{uri: 'https://picsum.photos/200/200'}}></Image>
                                 </ScrollView>
                                 <TouchableOpacity style={styles.view1}>
-                                    <Ionicons name='add-circle' size={40} color='black'/>
+                                    <Ionicons name='add-circle' size={40} color='black' />
                                 </TouchableOpacity>
                             </View>
                         </View>
-                        <View style={[styles.view2, {marginTop: 30}]}>
+                        <View style={[styles.view2, {marginTop: 10}]}>
                             <Text style={styles.text2}>Expense Details</Text>
+                            
+                                <View style={styles.des1}>
+                                    <View style={styles.pencil}>
+                                        <FontAwesome name='pencil' size={18} color='black'  />
+                                    </View>
+                                
+                                    <TextInput style={styles.input}
+                                            placeholder='Enter a description' name="desc"/>
+                                    {/* onChangeText={(text) => setDesc(text)} */}
+                                </View>
+                            
                             <View style={styles.des1}>
-                                <TextInput style={styles.input}
-                                           placeholder='Enter a description' name="desc"/>
-                                {/* onChangeText={(text) => setDesc(text)} */}
-                            </View>
-                            <View style={styles.des1}>
+                            <View style={styles.pencil}>
+                                        <FontAwesome name='dollar' size={18} color='black'  />
+                                    </View>
                                 <TextInput style={styles.input}
                                            placeholder='Amount' name="amt"/>
                                 {/* onChangeText={(text) => setAmt(text)} */}
                             </View>
                         </View>
-                        <View style={[styles.view2, {marginTop: 30}]}>
+                        <View style={[styles.view2, {marginTop: 10}]}>
                             <Text style={styles.text2}>Paid by</Text>
-                            <View style={[styles.view4, {marginTop: 30}]}>
+                            <View style={[styles.view4]}>
                                 <ScrollView style={styles.view3} horizontal={true}>
                                     <Image style={styles.img} source={{uri: 'https://picsum.photos/200/200'}}></Image>
                                     <Image style={styles.img} source={{uri: 'https://picsum.photos/200/200'}}></Image>
@@ -131,12 +151,33 @@ export default function AddExpense({navigation}) {
                                     <Ionicons name='add-circle' size={40} color='black'/>
                                 </TouchableOpacity></View>
                         </View>
-                        <View style={[styles.desre, {marginTop: 40}]}>
+                        <View style={styles.split}>
+                            <Text style={styles.text2}>Split Methods</Text>
+                            <View style={styles.arrow}>
+                            <Ionicons name='add-circle' size={30} color='black'/>
+                            </View>
+                        </View>
+
+                        <View style={[styles.desre, {marginTop: 20}]}>
+                            <TouchableOpacity style={styles.view5} onPress={pickImage}>
+                                <Text style={styles.textRe}>Add a Date</Text>
+                                <Ionicons name='add-circle' size={30} color='black'/>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={[styles.desre, {marginTop: 20}]}>
                             <TouchableOpacity style={styles.view5} onPress={pickImage}>
                                 <Text style={styles.textRe}>Add a receipt</Text>
                                 <Ionicons name='add-circle' size={30} color='black'/>
                             </TouchableOpacity>
                         </View>
+                        <View style={styles.des1}>
+                            <View style={styles.pencil} >
+                                <Ionicons name="bookmarks-outline" size={20}></Ionicons>
+                            </View>
+                                <TextInput style={styles.input}
+                                           placeholder='Add a comment' name="comment"/>
+                                {/* onChangeText={(text) => setAmt(text)} */}
+                            </View>
                         <View style={styles.btn}>
                             <TouchableOpacity onPress={handleSubmit}>
                                 <Text style={styles.btn1}>
@@ -153,24 +194,42 @@ export default function AddExpense({navigation}) {
 const styles = StyleSheet.create({
     bottomSheet: {
         height: '100%', //change this after design it
-        backgroundColor: COLORS.BG, width: '100%', borderTopEndRadius: 50, borderTopStartRadius: 50, marginTop: 10
-    }, container: {
-        paddingTop: 10, backgroundColor: COLORS.PRIMARY,
-    }, compTitle: {
-        marginTop: 30, justifyContent: "center", marginHorizontal: 30,
-    }, compTitleStyle: {
-        fontWeight: 'bold', fontSize: 25
-    }, firstContainer: {
+        backgroundColor: COLORS.BG, 
+        width: '100%', 
+        borderTopEndRadius: 50, 
+        borderTopStartRadius: 50, 
+        marginTop: 10
+    }, 
+    container: {
+        paddingTop: 10, 
+        backgroundColor: COLORS.PRIMARY,
+    }, 
+    compTitle: {
+        marginTop: 30, 
+        justifyContent: "center", 
+        marginHorizontal: 30,
+    }, 
+    compTitleStyle: {
+        fontWeight: 'bold', 
+        fontSize: 25
+    }, 
+    firstContainer: {
         width: '90%',
         borderRadius: 20,
         marginHorizontal: 18,
         paddingVertical: 10,
         paddingHorizontal: 20,
-        marginTop: 20,
+        marginTop: 5,
         flexDirection: 'column',
-    }, view1: {
-        marginLeft: 20, margin: 10, display: 'flex', justifyContent: 'center', alignItems: 'center',
-    }, img: {
+    }, 
+    view1: {
+        marginLeft: 20, 
+        margin: 5, 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+    }, 
+    img: {
         width: 50,
         height: 50,
         resizeMode: 'contain',
@@ -179,22 +238,46 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderRadius: 25,
         margin: 10,
-    }, text2: {
-        marginLeft: 10, color: 'black', fontWeight: "bold", marginTop: 10, fontSize: 16,
-    }, view2: {
-        width: '100%', flexDirection: 'column',
-    }, view3: {
-        width: '80%', flexDirection: 'row',
-    }, view4: {
-        width: '100%', flexDirection: 'row',
-    }, elevation: {
-        shadowColor: 'black', shadowOffset: {width: 1, height: 1}, elevation: 1, shadowOpacity: 0.4,
-    }, firstContainer2: {
-        height: 100, width: '90%', backgroundColor: 'white', borderRadius: 15, marginHorizontal: 18, marginTop: 20,
-    }, des1: {
-        flexDirection: 'row', justifyContent: 'flex-start'
-    }, input: {
-        marginTop: 10,
+    }, 
+    text2: {
+        marginLeft: 10, 
+        color: 'black', 
+        fontWeight: "bold", 
+        marginTop: 5, 
+        fontSize: 16,
+    }, 
+    view2: {
+        width: '100%', 
+        flexDirection: 'column',
+    }, 
+    view3: {
+        width: '80%', 
+        flexDirection: 'row',
+    }, 
+    view4: {
+        width: '100%', 
+        flexDirection: 'row',
+    }, 
+    elevation: {
+        shadowColor: 'black', 
+        shadowOffset: {width: 1, height: 1}, 
+        elevation: 1, 
+        shadowOpacity: 0.4,
+    }, 
+    firstContainer2: {
+        height: 100, 
+        width: '90%', 
+        backgroundColor: 'white', 
+        borderRadius: 15, 
+        marginHorizontal: 18, 
+        marginTop: 20,
+    }, 
+    des1: {
+        flexDirection: 'row', 
+        justifyContent: 'flex-start'
+    }, 
+    input: {
+        marginTop: 5,
         width: '100%',
         height: 50,
         borderColor: '#ccc',
@@ -202,19 +285,52 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         padding: 10,
         marginBottom: 10
-    }, desre: {
-        flexDirection: 'row', marginLeft: 15, justifyContent: "flex-end"
-    }, textRe: {
+    }, 
+    desre: {
+        flexDirection: 'row', 
+        marginLeft: 15, 
+        justifyContent: "flex-end"
+    }, 
+    textRe: {
         //textAlign:'center',
-        fontSize: 16, marginRight: 15
-    }, con: {
+        fontSize: 16, 
+        marginRight: 15
+    }, 
+    con: {
         paddingLeft: '50%',
-    }, view5: {
-        flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 20
-    }, btn1: {
-        borderWidth: 2, padding: 15, borderColor: 'black', color: 'white', backgroundColor: '#010B40',
-    }, btn: {
-        alignItems: 'flex-end', marginTop: 50, marginRight: 20, height: 50, marginBottom: 100
+    }, 
+    view5: {
+        flexDirection: 'row', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        marginTop: 2
+    }, 
+    btn1: {
+        borderWidth: 2, 
+        padding: 15, 
+        borderColor: 'black', 
+        color: 'white', 
+        backgroundColor: '#010B40',
+    }, 
+    btn: {
+        alignItems: 'flex-end', 
+        marginTop: 10, 
+        marginRight: 20, 
+        height: 50, 
+        marginBottom: 100
     },
+    pencil:{
+        marginLeft: 1,
+        marginTop:20,
+        marginRight:5
+    },
+    split:{
+       marginTop:20,
+       flexDirection:'row',
+       alignItems:'center'
+    },
+    arrow:{
+        marginLeft:'55%'
+    }
 
 });
